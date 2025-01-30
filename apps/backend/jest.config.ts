36751bf89d35ad349b87cb6a412f449e2ffe6d9e
Franco Eduardo Maurizzio (EXT) <Franco.Maurizzio+delsud@ext.delsud.com.ar>
2025-01-30 02:35:40 -0300

    se agrega dockerfile y .env example

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default {
   displayName: 'backend',
   preset: '../../jest.preset.js',
@@ -8,4 +9,24 @@ export default {
   moduleFileExtensions: ['ts', 'js', 'json'],
   coverageDirectory: '../../coverage/apps/backend',
   collectCoverage: true,
+  coverageReporters: ['text', 'html', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  verbose: true,
+  testTimeout: 30000,
+  maxWorkers: '50%',
+  errorOnDeprecated: true,
+  detectOpenHandles: true,
+  forceExit: true,
 };
